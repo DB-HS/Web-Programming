a9ea03eba1b02a0cb594f324c84687f66edd306b
유효상 <56590113+YuhyosangKr@users.noreply.github.com>
2021-08-30 11:28:17 +0900

    Update js.js

--- js.js
+++ js.js
@@ -59,3 +59,22 @@ var Body = {
 /* 참조: https://www.codingfactory.net/10438 */
 document.write( location.href );/* 현재 페이지 링크 가져오기(페이지 전환) */
 console.log( location.href )/* 콘솔 로그에 현재 페이지 링크 표시 */
+
+
+//<!-- 카테고리 매개변수 지우기 -->
+//<script type="text/javascript">		
+  //브라우저가 pushState가 지원되지 않는다면 사용하지 않도록 설정
+  if(typeof(history.pushState) == 'function')
+  {
+    //현재 주소를 가져온다.
+    var CatagoryURL = location.href;
+
+    //게시글 중 매개변수 category가 붙으면 없애버린다.
+    //CatagoryURL = CatagoryURL.replace(/\?category=([0-9]+)/ig,'');
+    CatagoryURL = CatagoryURL.replace(/\?[0-9, A-Z, a-z, =, \-, _]+/,'');
+
+    //페이지 새로 고침 없이 갱신
+    history.pushState(null, null, CatagoryURL);
+  }		
+</script>
+//<!-- // 카테고리 매개변수 지우기 -->
